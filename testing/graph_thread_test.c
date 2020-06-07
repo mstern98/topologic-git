f2be5dea6cfbe2f8b26e93d0e228c2e298db9617
BTMichalowicz <benjaminmichalowicz98@gmail.com>
2020-06-06 22:48:16 -0400

    Still working on graph vertex and edge testing

--- testing/graph_thread_test.c
+++ testing/graph_thread_test.c
@@ -2,17 +2,17 @@
 
 
 void test_graph_insert_vertex(struct graph*);
-void test_run_single_enum(struct graph*);
-void test_run_none_enum(struct graph*);
-void test_run_switch_enum(struct graph*);
-void test_graph_add_edge(struct graph*, struct edge**);
-void test_graph_add_bi_edge(struct graph*, struct edge**[]);
-void test_graph_modify_vertex(struct graph*, struct vertex*);
-void test_graph_modify_edge(struct graph*, struct edge*);
-void test_graph_modify_bi_edge(struct graph*, struct edge**);
-void test_graph_remove_vertex(struct graph*, struct vertex**);
-void test_graph_remove_edge(struct graph*, struct edge**);
-void test_graph_remove_bi_edge(struct graph*, struct edge**[]);
+//void test_run_single_enum(struct graph*);
+//void test_run_none_enum(struct graph*);
+//void test_run_switch_enum(struct graph*);
+void test_graph_add_edge(struct graph*);
+void test_graph_add_bi_edge(struct graph*);
+void test_graph_modify_vertex(struct graph*);
+void test_graph_modify_edge(struct graph*);
+void test_graph_modify_bi_edge(struct graph*);
+void test_graph_remove_vertex(struct graph*);
+void test_graph_remove_edge(struct graph*);
+void test_graph_remove_bi_edge(struct graph*);
 
 #define TEST_SIZE 100
 #define TEST_ARGC 3
@@ -25,6 +25,12 @@ struct vertex_result* testFunction(int argc, void* argv){
 	return res;
 }
 
+int testFuncEdge(int arg1, void* args2){
+
+	printf("Arg1: %d\n", arg1);
+	return 0;
+}
+
 
 
 int main(){
@@ -32,12 +38,25 @@ int main(){
 	assert(graph!=NULL);
 
 	fprintf(stderr, "BEGINNING GRAPH OPERATIONS: VERTICES\n");
-	/**Setup for vertex**/
-	//struct vertex** vertices =malloc(sizeof(struct vertex)*TEST_SIZE);
+		
+	//Adding
 	test_graph_insert_vertex(graph);
+	test_graph_add_edge(graph);
+	
+
+	//Modification
 
 
+	//Cleanup
+	test_graph_remove_vertex(graph);
 
+	//End it all;
+	destroy_graph(graph);
+	free(graph);
+	graph=NULL;
+	assert(graph==NULL);
+
+	return 0;
 }
 
 void test_graph_insert_vertex(struct graph* graph){
@@ -53,6 +72,33 @@ void test_graph_insert_vertex(struct graph* graph){
 	}
 	fprintf(stderr, "VERTEX INSERTION INTO GRAPH PASSED\n");
 }
+
+void test_graph_add_edge(struct graph* graph){
+	int i = 0;
+	for(i=0; i<TEST_SIZE; i++){
+		int id = i;
+		int argc=TEST_ARGC, glibc=TEST_ARGC;
+		int (*f)(int, void*) = &testFuncEdge;
+		void* glbl=malloc(32);
+		//assert(create_edge(
+		//
+		free(glbl);
+		glbl=NULL;
+
+	}
+	
+	fprintf(stderr,"EDGE INSERTION INTO GRAPH PASSED\n");
+}
+
+void test_graph_remove_vertex(struct graph* graph){
+	int i = 0;
+	for(i=0; i<TEST_SIZE; i++){	
+		struct vertex* v = (struct vertex* )find(graph->vertices, i);
+		assert(remove_vertex(graph, v)==0);
+	}
+	fprintf(stderr, "VERTEX REMOVAL FROM GRAPH PASSED\n");
+}
+
 
 		
 
